128bbd40b6a6212fcac15955bb794346ac011711
MatiasDonati <matod87@hotmail.com>
2021-09-21 19:43:41 -0300

    Modificacion app.js puerto para Heroku y Nuevo archivo Procfile

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
+// app.listen(3000, ()=>{
+//     console.log('Servidor funcionando en el 3000');
+// });
+
+app.listen(process.env.PORT || 3000, function(){
     console.log('Servidor funcionando en el 3000');
 });
+
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
